3b3c28463dd100fcf36834b8c94c72124fa5a5f8
Will Sutton <wsutton17@gmail.com>
2023-01-20 09:16:32 -0500

    interval polling auto-update example implemented

--- dash_basic.py
+++ dash_basic.py
@@ -19,51 +19,76 @@ from config import username
 from config import password
 from config import server
 
+import random
+from dash.dependencies import Input, Output
+
 
 app = dash.Dash(__name__)
 
-conn = pymssql.connect(server,username, password, database)
-cursor = conn.cursor()
+def get_data_sql():
+    conn = pymssql.connect(server,username, password, database)
+    cursor = conn.cursor()
+    query = f"SELECT * FROM {table}"
+    df = pd.read_sql(query, conn)
+    return df
 
-query = f"SELECT * FROM {table}"
+def get_data_sample():
+    i = random.randint(0,2)
+    df = pd.read_csv(f'data/sample-{i}')
+    print(i)
+    return df
 
-df = pd.read_sql(query, conn)
-
-cols = ['Weight', 'MPG.city', 'MPG.highway']
-for col in cols:
-    df[col] = df[col].astype('int')
-
-print(df.shape)
-print(df.dtypes)
-
-df2 = df[['Weight', 'MPG.city', 'MPG.highway']]
+def trim_cols(df):
+    cols = ['Weight', 'MPG.city', 'MPG.highway']
+    for col in cols:
+        df[col] = df[col].astype('int')
+    df2 = df[['Weight', 'MPG.city', 'MPG.highway']]
+    return df2
 
 
-# construct figures
-fig = px.scatter(df2, x='Weight', y='MPG.city', title='My first plot')
-fig2 = px.scatter(df2, x='Weight', y='MPG.highway', title='MPG (highway) vs Weight')
-fig2 = px.bar(df['Weight'])
+def make_figure_1():
+    df = get_data_sample()
+    df2 = trim_cols(df)
+    fig = px.scatter(
+                df2, 
+                x='Weight', 
+                y='MPG.city', 
+                title='My first plot'
+    )
+    return fig
 
 # put figures into dashboard's html
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash. Wills Dashboard!.'),
-    html.H1(children='Description of my project here.'),
 
     html.Div(children='''
         Dash: A web application framework for your data.
     '''),
 
     dcc.Graph(
-        id='first-graph',
-        figure=fig
+        id='fig-1',
+        figure=make_figure_1()
     ),
 
-    dcc.Graph(
-            id='example-graph',
-            figure=fig2
+    dcc.Interval(
+        id='interval-component',
+        interval=5*1000, # in milliseconds
+        n_intervals=0
     )
+
+    # dcc.Graph(
+    #         id='example-graph',
+    #         figure=fig2
+    # )
 ])
 
-# run the app
+@app.callback(
+    Output('fig-1', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def update_figure_1(n_intervals=None):
+    return make_figure_1()
+
+
 if __name__ == '__main__':
     app.run_server(debug=True )
