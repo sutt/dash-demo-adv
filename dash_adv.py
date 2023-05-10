3db6ad5594a1f6e7e4e3cf5e2a4baf08296bb97c
Will Sutton <wsutton17@gmail.com>
2023-05-10 16:00:28 -0400

    challenge complete

--- dash_adv.py
+++ dash_adv.py
@@ -24,6 +24,7 @@ from dash.dependencies import Input, Output
 
 
 app = dash.Dash(__name__)
+app.callback
 
 def get_data_sql():
     conn = pymssql.connect(server,username, password, database)
@@ -36,8 +37,8 @@ def get_data_full():
     df = pd.read_csv(f'data/Cars93.csv')
     return df
 
-def get_data_sample():
-    i = random.randint(0,2)
+def get_data_sample(i):
+    # i = random.randint(0,2)
     df = pd.read_csv(f'data/sample-{i}')
     print(i)
     return df
@@ -50,26 +51,28 @@ def trim_cols(df):
     return df2
 
 
-def make_figure_1():
-    df = get_data_sample()
+def make_figure_1(sample_num=0):
+    df = get_data_sample(sample_num % 3)
     df2 = trim_cols(df)
     fig = px.scatter(
                 df2, 
                 x='Weight', 
                 y='MPG.city', 
-                title='My first plot',
+                title=f'sample num: {sample_num % 3}',
     )
     return fig
 
-def make_figure_2(opacity=0.2):
+def make_figure_2(opacity=0.2, height=500):
     df = get_data_full()
     df2 = trim_cols(df)
+    print(f"====make_figure_2 is being called with opacity: {opacity} | height: {height}")
     fig = px.scatter(
                 df2, 
                 x='Weight', 
                 y='MPG.city', 
                 title=f'Second Plot with opacity of {opacity}',
                 opacity=opacity,
+                height=height,
     )
     return fig
 
@@ -88,13 +91,21 @@ app.layout = html.Div(children=[
 
     dcc.Interval(
         id='interval-component',
-        interval=5*1000, # in milliseconds
+        interval=1*1000, # in milliseconds
         n_intervals=0
     ),
     
+
+
     dcc.Input(
         value="0.2",
         id='opacity-input',
+        
+    ),
+
+    dcc.Input(
+        value="500",
+        id='height-input',
         
     ),
 
@@ -109,21 +120,46 @@ app.layout = html.Div(children=[
     Input('interval-component', 'n_intervals')
 )
 def update_figure_1(n_intervals=None):
-    return make_figure_1()
+    print(f"n_intervals {n_intervals}")
+    return make_figure_1(sample_num=n_intervals)
+
+# @app.callback(
+#     Output('fig-2', 'figure'),
+#     Input('opacity-input', 'value')
+# )
+# def update_opactiy(opacity_value):
+#     try:
+#         opacity_float = float(opacity_value)
+#     except:
+#         opacity_float = 0.5
+#     if (opacity_float > 1.0) or (opacity_float < 0.0):
+#         opacity_float = 1.0
+    
+#     return make_figure_2(opacity_float)
 
 @app.callback(
     Output('fig-2', 'figure'),
-    Input('opacity-input', 'value')
+    Input('height-input', 'value'),
+    Input('opacity-input', 'value'),
 )
-def update_opactiy(opacity_value):
+def update_height(height_value, opacity_value):
+    print(height_value, opacity_value)
+    
     try:
         opacity_float = float(opacity_value)
     except:
-        opacity_float = 1.0
+        opacity_float = 0.5
     if (opacity_float > 1.0) or (opacity_float < 0.0):
         opacity_float = 1.0
     
-    return make_figure_2(opacity_float)
+    try:
+        height_value = int(height_value)
+    except:
+        height_value = 500
+    
+    return make_figure_2(opacity=opacity_float ,height=height_value)
+
+
 
 
 if __name__ == '__main__':
